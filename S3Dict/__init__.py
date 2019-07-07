15b18538c5dfda94a502914ff0a70c380861d19a
Rhet Turnbull <rturnbull@gmail.com>
2019-07-07 12:57:05 -0700

    Moved to module, added example.py

--- S3Dict/__init__.py
+++ S3Dict/__init__.py
@@ -1,5 +1,4 @@
 import collections
-import configparser
 import traceback
 import json
 
@@ -8,29 +7,6 @@ import time
 from loguru import logger
 import boto3
 from botocore.client import Config
-
-def load_config(configfile):
-    config = configparser.ConfigParser()
-    config.read(configfile)
-
-    if "twitter" not in config or "aws" not in config:
-        quit(f"Error parsing {configfile}")
-
-    c = {}
-    try:
-        c["CONSUMER_KEY"] = config["twitter"]["CONSUMER_KEY"]
-        c["CONSUMER_SECRET"] = config["twitter"]["CONSUMER_SECRET"]
-        c["ACCESS_TOKEN"] = config["twitter"]["ACCESS_TOKEN"]
-        c["ACCESS_TOKEN_SECRET"] = config["twitter"]["ACCESS_TOKEN_SECRET"]
-
-        c["AWS_ACCESS_KEY_ID"] = config["aws"]["ACCESS_KEY_ID"]
-        c["AWS_ACCESS_SECRET_KEY"] = config["aws"]["ACCESS_SECRET_KEY"]
-        c["AWS_BUCKET_NAME"] = config["aws"]["BUCKET_NAME"]
-    except Exception as e:
-        traceback.print_exc()
-        quit(f"Error parsing {configfile}: {e}")
-
-    return c
 
 
 class S3Dict(collections.UserDict):
@@ -67,7 +43,7 @@ class S3Dict(collections.UserDict):
             config=Config(signature_version="s3v4"),
         )
 
-        #TODO: logic to either load state or preserve state depending on data
+        # TODO: logic to either load state or preserve state depending on data
         self.__loadstate()
         self.data = data
         self.__savestate()
@@ -78,22 +54,28 @@ class S3Dict(collections.UserDict):
         fnames = list(map(lambda x: x.key, objs))
         logger.debug(f"fnames: {fnames}")
 
-        if (self.__fname in fnames):
+        if self.__fname in fnames:
             try:
-                obj = self.__s3.Object(self.__BUCKET_NAME,self.__fname)
-                data = obj.get()['Body'].read()
+                obj = self.__s3.Object(self.__BUCKET_NAME, self.__fname)
+                data = obj.get()["Body"].read()
                 state = json.loads(data)
                 logger.debug(f"data: {data}, state: {state}")
             except Exception as e:
-                logger.debug(f"Error loading state from {self.__s3}: {e} {traceback.print_exc}") 
+                logger.debug(
+                    f"Error loading state from {self.__s3}: {e} {traceback.print_exc}"
+                )
                 # couldn't find the state file on s3 bucket so initialize state
         else:
             try:
                 logger.debug(f"{self.__fname} not in bucket, creating it")
                 data = json.dumps(state)
-                self.__s3.Bucket(self.__BUCKET_NAME).put_object(Key=self.__fname, Body=data)
+                self.__s3.Bucket(self.__BUCKET_NAME).put_object(
+                    Key=self.__fname, Body=data
+                )
             except Exception as e:
-                logger.debug(f"Error creating {self.__fname} {self.__s3}: {e} {traceback.print_exc}") 
+                logger.debug(
+                    f"Error creating {self.__fname} {self.__s3}: {e} {traceback.print_exc}"
+                )
 
         self.data = state
 
@@ -104,9 +86,9 @@ class S3Dict(collections.UserDict):
             data = json.dumps(self.data)
             self.__s3.Bucket(self.__BUCKET_NAME).put_object(Key=self.__fname, Body=data)
         except:
-            quit(f"Error saving state to {self.__s3}") 
+            quit(f"Error saving state to {self.__s3}")
         stop = time.clock()
-        elapsed = stop-start
+        elapsed = stop - start
         logger.debug(f"elapsed: {elapsed}")
 
     def fromkeys(self, *args):
@@ -141,48 +123,3 @@ class S3Dict(collections.UserDict):
         logger.debug(f"__delitem__: {key}")
         del (self.data[key])
         self.__savestate()
-
-
-if __name__ == "__main__":
-    config = load_config("proverb-bot.ini")
-    state = S3Dict(
-        bucket_name=config["AWS_BUCKET_NAME"],
-        access_key_id=config["AWS_ACCESS_KEY_ID"],
-        access_secret_key=config["AWS_ACCESS_SECRET_KEY"],
-        file_name="test.json",
-    )
-    state["foo"] = "bar"
-    print(f"foo: {state['foo']}")
-
-    if "foobar" in state:
-        print("Yep")
-    else:
-        print("Nope")
-
-    state["foobar"] = 42
-    for k in state:
-        print(f"k = {k}, {state[k]}")
-
-    for k in state.keys():
-        print(f"k = {k}, {state[k]}")
-
-    del (state["foobar"])
-
-    for k, v in state.items():
-        print(k, v)
-
-    state["foo"] = "bar"
-    state["answer"] = 42
-
-    print(len(state))
-    state.clear()
-
-    state = state.fromkeys([1, 2, 3])
-
-    print(state.pop(1))
-    print(state.popitem())
-    print(state.setdefault("42"))
-    state.update({"a": "1", "b": 2, "c": 3})
-    state["foobar"]="hahhah"
-    print(state.values())
-    print(f"keys = {state.keys()}")
---
+++ example.py
@@ -0,0 +1,70 @@
+import configparser
+import traceback
+
+from loguru import logger
+
+import S3Dict
+
+
+def load_config(configfile):
+    config = configparser.ConfigParser()
+    config.read(configfile)
+
+    if "aws" not in config:
+        quit(f"Error parsing {configfile}: missing [aws] section")
+
+    c = {}
+    try:
+        c["AWS_ACCESS_KEY_ID"] = config["aws"]["ACCESS_KEY_ID"]
+        c["AWS_ACCESS_SECRET_KEY"] = config["aws"]["ACCESS_SECRET_KEY"]
+        c["AWS_BUCKET_NAME"] = config["aws"]["BUCKET_NAME"]
+    except Exception as e:
+        traceback.print_exc()
+        quit(f"Error parsing {configfile}: {e}")
+
+    return c
+
+
+if __name__ == "__main__":
+    config = load_config("example.ini")
+    state = S3Dict.S3Dict(
+        bucket_name=config["AWS_BUCKET_NAME"],
+        access_key_id=config["AWS_ACCESS_KEY_ID"],
+        access_secret_key=config["AWS_ACCESS_SECRET_KEY"],
+        file_name="test.json",
+    )
+    state["foo"] = "bar"
+    print(f"foo: {state['foo']}")
+
+    if "foobar" in state:
+        print("Yep")
+    else:
+        print("Nope")
+
+    state["foobar"] = 42
+    for k in state:
+        print(f"k = {k}, {state[k]}")
+
+    for k in state.keys():
+        print(f"k = {k}, {state[k]}")
+
+    del (state["foobar"])
+
+    for k, v in state.items():
+        print(k, v)
+
+    state["foo"] = "bar"
+    state["answer"] = 42
+
+    print(len(state))
+    state.clear()
+
+    state = state.fromkeys([1, 2, 3])
+
+    print(state.pop(1))
+    print(state.popitem())
+    print(state.setdefault("42"))
+    state.update({"a": "1", "b": 2, "c": 3})
+    state["foobar"] = "hahhah"
+    print(state.values())
+    print(f"keys = {state.keys()}")
